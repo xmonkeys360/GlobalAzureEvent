d9ca9b893e5b6896b689107b1aba66b9efb8f042
Anbu Mani <anbumani91@hotmail.com>
2021-04-01 16:39:28 +0530

    Updated Speakers List

--- GA2021/assets/js/main.js
+++ GA2021/assets/js/main.js
@@ -20,7 +20,7 @@
     /* ==========================================================================
        countdown timer
        ========================================================================== */
-     jQuery('#clock').countdown('2021/04/17',function(event){
+     jQuery('#clock').countdown('2021/04/16',function(event){
       var $this=jQuery(this).html(event.strftime(''
       +'<div class="time-entry days"><span>%-D</span> Days</div> '
       +'<div class="time-entry hours"><span>%H</span> Hours</div> '
