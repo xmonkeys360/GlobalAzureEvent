943118fcccdc9dc3479092eb81d77843e0141e39
Anbu Mani <anbumani91@hotmail.com>
2021-05-12 16:54:52 +0530

    Removed Popup

--- GA2021/assets/js/main.js
+++ GA2021/assets/js/main.js
@@ -31,16 +31,16 @@
   /* ==========================================================================
      Youtube Popup
      ========================================================================== */
-      $(document).ready(function () {
-          //$("#delCookie").click(function(){
-          //    del_cookie("cookie");
-          //});
-          $("#exampleModalCenter").modal("show");
-          var expire = new Date();
-          expire = new Date(expire.getTime() + 7776000000);
-          document.cookie = "cookie=here; expires=" + expire;
+      //$(document).ready(function () {
+      //    //$("#delCookie").click(function(){
+      //    //    del_cookie("cookie");
+      //    //});
+      //    $("#exampleModalCenter").modal("show");
+      //    var expire = new Date();
+      //    expire = new Date(expire.getTime() + 7776000000);
+      //    document.cookie = "cookie=here; expires=" + expire;
 
-      });
+      //});
 
 
     /* slicknav mobile menu active  */
